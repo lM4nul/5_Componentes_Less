450db9ac9b690013363f18053fdff8f76c0ff077
AngelLagos <angelagosauceda@gmail.com>
2026-07-04 18:11:59 -0600

    feat: add heropanel section and update styles

--- src/data/index.js
+++ src/data/index.js
@@ -1,9 +1,10 @@
 import navbar from './pages/navbar_page'
+import hero from './pages/hero_page'
 
-export default (page)=>{
-    switch(page){
+export default (page) => {
+    switch(page) {
         case '/index.html':
-            return navbar;
+            return { ...navbar, ...hero };
         default:
             return {};
     }
