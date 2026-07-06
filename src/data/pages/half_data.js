b70df32d2d8814d050e14d63a56b5f7f6c4f97d0
lsofiaort <lizs_ortizc37@unicah.edu>
2026-07-05 23:37:29 -0600

    half_panel

---
+++ src/data/pages/half_data.js
@@ -0,0 +1,11 @@
+export default {
+    half_information: [
+
+        {
+            info: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim cupiditate corporis sequi? Deserunt nam aliquam, eius, facilis officiis, doloremque ratione minus aperiam voluptas sit voluptate libero alias itaque quidem laudantium!",
+            imagen: "https://picsum.photos/id/28/400/300",
+            label: "imagen 1",
+        },
+
+    ]
+}
