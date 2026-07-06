84bd800bda9df42f7f168ba374a6b7e86bd5914d
Elieth-Amador <aeliethamador20@gmail.com>
2026-07-05 23:41:58 -0600

    creation footer

--- src/data/index.js
+++ src/data/index.js
@@ -1,10 +1,11 @@
 import navbar from './pages/navbar_page'
 import hero from './pages/hero_page'
+import footer from './pages/footer_page'
 
 export default (page) => {
     switch(page) {
         case '/index.html':
-            return { ...navbar, ...hero };
+            return { ...navbar, ...hero, ...footer};
         default:
             return {};
     }
---
+++ src/data/pages/footer_page.js
@@ -0,0 +1,44 @@
+export default {
+    footer: {
+        empresa: {
+            nombre: "Page",
+            descripcion: "Creamos experiencias digitales modernas ofreciendo soluciones tecnológicas de calidad."
+        },
+
+        contactos: [
+            {
+                icono: "img",
+                texto: "San Pedro Sula, Honduras"
+            },
+            {
+                icono: "img",
+                texto: "+504 9876-5432"
+            },
+            {
+                icono: "img",
+                texto: "contacto@page.com"
+            }
+        ],
+
+        enlaces: [
+            {
+                nombre: "Inicio",
+                url: "#"
+            },
+            {
+                nombre: "Servicios",
+                url: "#"
+            },
+            {
+                nombre: "Nosotros",
+                url: "#"
+            },
+            {
+                nombre: "Contacto",
+                url: "#"
+            }
+        ],
+
+        copyright: "grupo E"
+    }
+};
